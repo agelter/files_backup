028bdd86f723d01bfdb7e82a273f00aed8c4646d
Aaron Gelter <agelter@netflix.com>
2024-11-13 14:26:38 -0800

    Fix vscode imports when vscode isn't being installed

--- home.nix
+++ home.nix
@@ -27,7 +27,7 @@ in
     stateVersion = "23.05";
   };
 
-  imports = [
+  imports = if withGUI then [
     # Source: https://gist.github.com/piousdeer/b29c272eaeba398b864da6abf6cb5daa
     # Make vscode settings writable
 
@@ -40,7 +40,7 @@ in
       url = "https://gist.githubusercontent.com/piousdeer/b29c272eaeba398b864da6abf6cb5daa/raw/41e569ba110eb6ebbb463a6b1f5d9fe4f9e82375/vscode.nix";
       sha256 = "fed877fa1eefd94bc4806641cea87138df78a47af89c7818ac5e76ebacbd025f";
     }) { inherit config lib pkgs; })
-  ];
+  ] else [];
 
   home.file.".npmrc".source = ./configs/.npmrc;
   home.file.".prettierrc".source = ./configs/.prettierrc;

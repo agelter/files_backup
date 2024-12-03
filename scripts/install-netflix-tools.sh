db5598a2ec60954e209ac395f0876bc46a09ae04
Aaron Gelter <agelter@netflix.com>
2024-12-03 14:57:29 -0800

    Manually isntall git-clang-format because nix's clang-tools doesn't include it

--- scripts/install-netflix-tools.sh
+++ scripts/install-netflix-tools.sh
@@ -8,3 +8,10 @@ curl -s 'https://artifacts.netflix.com/devtools/metatron-cli/install-script' | b
 
 # dropship
 curl -sL https://go.prod.netflix.net/install-vscode-dropship | bash
+
+# git-clang-format
+# specific SHA because the most recent version of git-clang-format
+# is not compatible with the version of clang-format (16.0.6) installed by nix
+curl -sL https://raw.githubusercontent.com/llvm/llvm-project/aeaae5311b8d4bbcfe7ef5cff722ea36b038c0ad/clang/tools/clang-format/git-clang-format \
+    -o "${HOME}/bin/git-clang-format"
+chmod +x "${HOME}/bin/git-clang-format"

19e13c104b38774e8de0d0c981c3bb9b8caf976e
Aaron Gelter <agelter@netflix.com>
2025-03-21 09:24:28 -0700

    Clean up install.sh to allow multiple options

--- install.sh
+++ install.sh
@@ -10,13 +10,34 @@ if [ -z "${1:-}" ]; then
   exit 1
 fi
 
-if [ -n "${3:-}" ]; then
-  echo "Only one flag (--update or --clean) is allowed"
-  exit 1
-fi
-
 CONFIG="$1"
-FLAG="${2:-''}"
+shift
+
+# Initialize flags
+UPDATE_FLAG=false
+CLEAN_FLAG=false
+DEBUG_FLAG=false
+
+# Process all remaining arguments as flags
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --update)
+      UPDATE_FLAG=true
+      ;;
+    --clean)
+      CLEAN_FLAG=true
+      ;;
+    --debug)
+      DEBUG_FLAG=true
+      ;;
+    *)
+      echo "Unknown flag: $1"
+      echo "Supported flags: --update, --clean, --debug"
+      exit 1
+      ;;
+  esac
+  shift
+done
 
 # hack because nix doesn't want to run scripts directly
 # Run the newt installation script
@@ -45,7 +66,7 @@ trap cleanup EXIT SIGINT SIGTERM
 # end hack
 
 if [[ "$(uname)" == "Darwin" ]]; then
-  if [ -e /nix ] && [[ "$FLAG" == "--clean" ]]; then
+  if [ -e /nix ] && [[ "$CLEAN_FLAG" == "true" ]]; then
     echo "Cleaning up old nix installation"
     zsh scripts/uninstall_nix_macos.zsh
     exit 0
@@ -56,7 +77,7 @@ fi
 if ! command -v nix > /dev/null; then
   echo "Installing nix..."
   if [[ "$(uname)" == "Darwin" ]]; then
-    if [ -e /nix ] && [[ "$FLAG" == "--clean" ]]; then
+    if [ -e /nix ] && [[ "$CLEAN_FLAG" == "true" ]]; then
       echo "Cleaning up old nix installation"
       zsh scripts/uninstall_nix_macos.zsh
     fi
@@ -90,15 +111,18 @@ if [[ "$(uname)" == "Darwin" ]]; then
 fi
 
 # init!
-if [[ "$FLAG" == "--update" ]]; then
+if [[ "$UPDATE_FLAG" == "true" ]]; then
     echo "Updating flake.lock with latest packages"
     nix flake update
 fi
-home-manager --debug -v switch --flake .#"${CONFIG}" -b bk
+
+# Use parameter expansion for debug flag
+DEBUG_PARAM=""
+[[ "$DEBUG_FLAG" == "true" ]] && DEBUG_PARAM="--debug"
+home-manager $DEBUG_PARAM switch --flake .#"${CONFIG}" -b bk
 
 # add zsh as a login shell
 command -v zsh | sudo tee -a /etc/shells
 
 # use zsh as default shell
 sudo chsh -s "$(which zsh)" "$USER"
-
